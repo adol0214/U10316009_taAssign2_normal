21dba44e40f652a38742cbac4e4db1c82f8dc3f3
adol0214 <s910714tw@gmail.com>
2015-05-16 22:24:12 +0800

    拆開的第二部分

---
+++ Circle.java
@@ -0,0 +1,14 @@
+public class Circle{
+	int Radius;
+	Circle(){
+		Radius = 0;
+	}
+	
+	double CirArea(int Rad){
+		return Rad * Rad *3.14;
+	}
+	
+	double CirPar(int Rad){
+		return 2 * Rad * 3.14;
+	}
+}
